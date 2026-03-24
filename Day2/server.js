b935e9f9db9b92265b142cf3c8352537bc7480d2
Hariomptdr <patidarh178@gmail.com>
2026-03-25 03:23:50 +0530

    learn about the API and created a notes API

--- Day2/server.js
+++ Day2/server.js
@@ -4,5 +4,12 @@ const app = express()
 app.get("/", (req, res) =>{
     res.send("Hello World")
 })
+app.get("/users", (req, res) => {
+    res.status(200).json([
+        { id: 1, name: "Hariom" },
+        { id: 2, name: "Harman" }
+    ])
+})
+
 
 app.listen(3000);
